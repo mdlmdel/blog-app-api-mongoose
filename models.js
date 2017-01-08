7ec8f11662429b48dc08d04bee71dda0d88408d9
MdL <mdlmdel@gmail.com>
2017-01-07 23:56:57 -0500

    Initial commit

---
+++ models.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const blogPostSchema = mongoose.Schema({
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const {DATABASE_URL, PORT} = require('./config');
+const {BlogPost} = require('./models');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+
